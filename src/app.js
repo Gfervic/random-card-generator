a9beb9a7f82baf00ffc71d915af3510756a350f5
@betagon <gon.fervic@gmail.com>
2022-01-07 23:46:32 +0000

    implemented the whole thing but having troubles implementing the btn

--- src/app.js
+++ src/app.js
@@ -2,10 +2,42 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+
+const suits = ["heart", "diamond", "club", "spade"];
+
+const cards = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+let randSuits = Math.floor(Math.random() * 4);
+let randCards = Math.floor(Math.random() * 13);
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardNumber = cards[randCards];
+  let suit = suits[randSuits];
+  let card = document.getElementById("card");
+  card.innerHTML = cardNumber;
+  card.classList.add(suit);
 };
+
+function myFunction() {
+  let cardNumber = cards[randCards];
+  let suit = suits[randSuits];
+  let card = document.getElementById("card");
+  card.innerHTML = cardNumber;
+  card.classList.add(suit);
+}
